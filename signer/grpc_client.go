954dc86974d89156aa2996246946192e596ed299
Andrew Gouin <andrew@gouin.io>
2025-03-27 14:35:33 -0600

    lint

--- signer/grpc_client.go
+++ signer/grpc_client.go
@@ -27,7 +27,7 @@ func NewHorcruxGRPCClient(
 	logger *slog.Logger,
 	address string,
 ) (*HorcruxGRPCClient, error) {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
